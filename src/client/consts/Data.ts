d319f9d266efef2004b01ae24d4776148ae88fae
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2019-12-24 01:19:52 +0900

    refactor(client): なんちゃってatomic design

---
+++ src/client/consts/Data.ts
@@ -0,0 +1,6 @@
+export interface Data {
+  time: string;
+  temperature: number;
+  humidity: number;
+  pressure: number;
+}
---
+++ src/client/organisms/Header.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export const Header: React.FunctionComponent = () => {
+  return (
+    <>
+      <h1>今日のちゃおハウス</h1>
+      <p>
+        <a href="https://github.com/chao7150/house-temperature-web">
+          ソースコード
+        </a>
+      </p>
+    </>
+  );
+};
