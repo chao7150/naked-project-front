3ac8f51e0dfb6d5932ebfc4a0f6a3a776206444d
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2020-02-23 12:27:59 +0900

    perf: bundleサイズ計測、locale周りの削減

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,10 +1,25 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: "packages/client/ui/index.html",
 });
+
+const bundleAnalyzerPlugin = new BundleAnalyzerPlugin({
+  analyzerMode: "static",
+  reportFilename: "../logs/report.html",
+  openAnalyzer: false,
+});
+
+const normalModuleReplacementPlugin = new webpack.NormalModuleReplacementPlugin(
+  /moment-timezone\/data\/packed\/latest\.json/,
+  path.resolve(__dirname, "../../misc/timezone-definitions/latest.json"),
+);
+
+const ignorePlugin = new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/);
 
 // ここで何も設定しなくてもprocess.env.NODE_ENVはインジェクトされる
 const definePlugin = new webpack.DefinePlugin({});
@@ -30,7 +45,13 @@ module.exports = {
       },
     ],
   },
-  plugins: [htmlWebpackPlugin, definePlugin],
+  plugins: [
+    htmlWebpackPlugin,
+    definePlugin,
+    bundleAnalyzerPlugin,
+    normalModuleReplacementPlugin,
+    ignorePlugin,
+  ],
   resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },
   devServer: {
     contentBase: `${__dirname}/dist`,
