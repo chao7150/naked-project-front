d006c9dfcf6793d216ceee96dd2ec987c40d478e
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2020-03-29 17:07:25 +0900

    build: 自動デプロイ修正

--- scripts/getAccessToken.ts
+++ scripts/getAccessToken.ts
@@ -2,19 +2,17 @@ const axios = require("axios");
 
 const execSync = require("child_process").execSync;
 
-const conf = require("../config/conf.json");
-
-const payload = `grant_type=authorization_code&code=${conf.code}&redirect_uri=${conf.redirectUri}`;
+const payload = `grant_type=authorization_code&code=${process.env.CODE}&redirect_uri=${process.env.REDIRECT_URI}`;
 const encodedIdAndSecret = Buffer.from(
-  `${conf.clientId}:${conf.clientSecret}`,
+  `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`,
 ).toString("base64");
 console.log(
   `curl -X POST -H "Authorization: Basic ${encodedIdAndSecret}" -d "${payload}" https://accounts.spotify.com/api/token`,
 );
 const params = new URLSearchParams();
 params.append("grant_type", "authorization_code");
-params.append("code", conf.code);
-params.append("redirect_uri", conf.redirectUri);
+params.append("code", process.env.CODE!);
+params.append("redirect_uri", process.env.REDIRECT_URI!);
 axios
   .post("https://accounts.spotify.com/api/token", params, {
     headers: {
